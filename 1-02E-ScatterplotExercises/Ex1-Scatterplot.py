be6151d7f97758767f291f03a296551d4f3846d4
evapanizo <panizo.eva@gmail.com>
2020-02-01 20:05:31 +0100

    Exercises-PART1-Scatter-Completed

--- 1-02E-ScatterplotExercises/Ex1-Scatterplot.py
+++ 1-02E-ScatterplotExercises/Ex1-Scatterplot.py
@@ -7,21 +7,26 @@
 ######
 
 # Perform imports here:
-
-
-
+import plotly.offline as pyo
+import plotly.graph_objs as go
+import numpy as np
 
 # Define a data variable
-
-
-
-
-
+random_x = np.random.randn(1000)
+random_y = np.random.rand(1000)
+data = [go.Scatter(x = random_x, 
+                   y = random_y, 
+                   mode = 'markers',
+                   marker = dict(size = 8, 
+                                 color = 'rgb(7, 180, 255)')
+                   )
+        ]
 
 # Define the layout
-
-
-
-
+layout = go.Layout(title = "X VS Y", 
+                   xaxis = dict(title = "X"), 
+                   yaxis = dict(title = "Y"))
 
 # Create a fig from data and layout, and plot the fig
+fig = go.Figure(data = data, layout = layout)
+pyo.plot(fig, filename='./1-02E-ScatterplotExercises/my_solution.html')

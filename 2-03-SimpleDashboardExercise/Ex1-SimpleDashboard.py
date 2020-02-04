25512f633fb25fd56798a511e6ad45faf944c5e6
evapanizo <panizo.eva@gmail.com>
2020-02-04 10:53:31 +0100

    Exercises-PART2-SimpleDashboard-Completed

--- 2-03-SimpleDashboardExercise/Ex1-SimpleDashboard.py
+++ 2-03-SimpleDashboardExercise/Ex1-SimpleDashboard.py
@@ -8,22 +8,32 @@
 ######
 
 # Perform imports here:
-
-
-
-
-
+import dash
+import pandas as pd
+import plotly.graph_objects as go
+import dash_core_components as dcc
+import dash_html_components as html
 
 # Launch the application:
-
+app = dash.Dash()
 
 # Create a DataFrame from the .csv file:
-
+df = pd.read_csv("./Data/OldFaithful.csv")
 
 # Create a Dash layout that contains a Graph component:
+data = [go.Scatter(x=df['X'], y=df['Y'], mode="markers", marker=dict(size=12, color="blue"))]
+layout = go.Layout(title="Old Faithful Eruption Interval VS Duration",
+                   xaxis=dict(title="Eruption duration (minutes)"),
+                   yaxis=dict(title="Interval to next eruption (minutes)"),
+                   hovermode="closest")
 
+app.layout = html.Div([
+    html.H1("My First Simple Dashboard", id="page-header"),
+    dcc.Graph(id="first-graph", figure=dict(data=data, layout=layout))
+])
 
-
+if __name__ == "__main__":
+    app.run_server()
 
 
 
